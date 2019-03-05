d138ed1b5ebfab72b48239bb605c72eccec7312d
Antony <34961906+Antstoe@users.noreply.github.com>
2019-03-05 10:15:40 -0800

    Modified html-routes to include index

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,11 +9,17 @@ var app = express();
 var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 // HTML ROUTES
-
+//Get for Survey
 app.get("/", function (req, res) {
     // res.sendFile(path.join(__dirname, "home.html"));
     res.sendFile('survey.html', { root: "./views/"});
 });
+
+//Get for Index
+// Route for Index
+app.use('./views/index', function(req, res){
+	res.sendFile(path.join(__dirname + './views/index.html'));
+	});
 
 // app.get("/survey", function (req, res) {
 //     res.sendFile('survey.html', { root: "./app/public/"});
