ec342b76ab236d5fc3bf6a3d4c3fa5227e2b8036
ymsebast <ymsebast@uci.edu>
2019-03-06 21:19:10 -0800

    heroku connection fixed

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  password: "root",
-  database: "proj2_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3307,
+//   user: "root",
+//   password: "root",
+//   database: "proj2_db"
+// });
+var connection;
+if (process.env.JAWSDB_URL) {
+  // Database is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // Database is local
+  connection = mysql.createConnection({
+      port: 3306,
+      host: 'localhost',
+      user: 'tvqp7z8wsb4d6vyv',
+      password: 'elbmnsp4t9dyp3s3',
+      database: 'proj2_db'
+  })
+};
 
 // Make connection.
 connection.connect(function(err) {
